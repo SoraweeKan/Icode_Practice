8de9adeb37bfe9cffd57608ce7e4b0cd3454e7bd
SoraweeKan <65585968+SoraweeKan@users.noreply.github.com>
2020-05-21 17:02:12 +0700

    32

--- Easy_Problem.py
+++ Easy_Problem.py
@@ -256,9 +256,9 @@ def problem29(x):
 def problem32 (x) :
     for i in range(1,x+1) :
         if i % 3 == 0 and i % 15 != 0:
-            print("x", )
+            print("x", end=" ")
         elif i % 5 == 0 and i % 15 != 0:
-            print("y", )
+            print("y", end=" ")
         elif i % 15 == 0 :
-            print("z", )
-        else : print(i)        
+            print("z", end=" ")
+        else : print(i, end=" ")    
